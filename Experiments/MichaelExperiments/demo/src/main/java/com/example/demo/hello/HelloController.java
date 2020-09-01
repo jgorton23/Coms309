d29392c780fc075e0edd484c970818d1ea2123f1
mamoore <mamoore@iastate.edu>
2020-08-31 19:12:21 -0500

    added models to mciahelexperiment

--- Experiments/MichaelExperiments/demo/src/main/java/com/example/demo/hello/HelloController.java
+++ Experiments/MichaelExperiments/demo/src/main/java/com/example/demo/hello/HelloController.java
@@ -3,11 +3,15 @@ package com.example.demo.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.modelHello;
+
 @RestController
 public class HelloController {
 	
+	private modelHello helloMessage = new modelHello("Hello, this statement was made by an model(?).");
+	
 	@RequestMapping("/hi")
 	public String sayHi() {
-		return "hi";
+		return helloMessage.getMessage();
 	}
 }
---
+++ Experiments/MichaelExperiments/demo/src/main/java/com/example/demo/model/modelHello.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class modelHello {
+	private String message;
+	
+	public modelHello(String message){
+		this.message = message;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+}
