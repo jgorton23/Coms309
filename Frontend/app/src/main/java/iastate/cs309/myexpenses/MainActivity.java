842bfc4880fe9628cdc07ce4c064d136f9f5da6d
Polina <psemenova0201@gmail.com>
2020-09-16 16:16:06 -0500

    Play around with navigation and buttons

---
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/MainActivity.java
@@ -0,0 +1,16 @@
+package iastate.cs309.myexpenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login_screen);
+
+
+    }
+}
