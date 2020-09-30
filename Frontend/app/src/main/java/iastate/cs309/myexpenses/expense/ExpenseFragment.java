ae15e48ea88e50ebcb96f2af843ee5ba467e2fb3
Kateryna <kateryna@iastate.edu>
2020-09-30 17:50:24 -0500

    Last navigation bar button leads to screen displaying the items from the server.

--- Frontend/app/src/main/java/iastate/cs309/myexpenses/expense/ExpenseFragment.java
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/expense/ExpenseFragment.java
@@ -1,6 +1,9 @@
 package iastate.cs309.myexpenses.expense;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,7 +14,25 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import iastate.cs309.myexpenses.Expense;
+import iastate.cs309.myexpenses.ItemListActivity;
 import iastate.cs309.myexpenses.R;
 import iastate.cs309.myexpenses.expense.dummy.DummyContent;
 
@@ -60,13 +81,83 @@ public class ExpenseFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
+            loadData(recyclerView);
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new MyExpenseRecyclerViewAdapter(DummyContent.ITEMS));
+//            recyclerView.setAdapter(new MyExpenseRecyclerViewAdapter(DummyContent.ITEMS));
         }
         return view;
     }
+
+    private void loadData(final RecyclerView recyclerView) {
+        String url = "http://coms-309-ug-02.cs.iastate.edu:8080/items";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        ArrayList<Expense> listdata = new ArrayList<Expense>();
+                        if (response != null) {
+                            //while the server doesn't work
+                            if (response.length() == 0) {
+                                Expense item = new Expense(
+                                        "123",
+                                        "note1",
+                                        "note1",
+                                        new BigDecimal(10),
+                                        "gas");
+                                listdata.add(item);
+                            }
+                            for (int i=0;i<response.length();i++){
+                                try {
+                                    JSONObject jsonObject = response.getJSONObject(i);
+                                    Expense item = new Expense(
+                                            jsonObject.getString("id"),
+                                            jsonObject.getString("notes"),
+                                            jsonObject.getString("notes"),
+                                            new BigDecimal(jsonObject.getDouble("price")),
+                                            jsonObject.getString("catagory"));
+                                    listdata.add(item);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+//                        recyclerView.setAdapter(new ItemListActivity.SimpleItemRecyclerViewAdapter(ExpenseFragment.this, listdata, false));
+                        recyclerView.setAdapter(new MyExpenseRecyclerViewAdapter(listdata, getActivity()));
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        System.out.println(error);
+                        new AlertDialog.Builder(getContext())
+                                .setTitle("Error")
+                                .setMessage(error.toString())
+
+                                // Specifying a listener allows you to take an action before dismissing the dialog.
+                                // The dialog is automatically dismissed when a dialog button is clicked.
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        // Continue with delete operation
+                                    }
+                                })
+
+                                // A null listener allows the button to dismiss the dialog and take no further action.
+                                //.setNegativeButton(android.R.string.no, null)
+                                //.setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+        requestQueue.add(jsonArrayRequest);
+    }
+
 }
--- Frontend/app/src/main/java/iastate/cs309/myexpenses/expense/MyExpenseRecyclerViewAdapter.java
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/expense/MyExpenseRecyclerViewAdapter.java
@@ -1,14 +1,20 @@
 package iastate.cs309.myexpenses.expense;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import iastate.cs309.myexpenses.Expense;
 import iastate.cs309.myexpenses.R;
 import iastate.cs309.myexpenses.expense.dummy.DummyContent.DummyItem;
+import iastate.cs309.myexpenses.ItemDetailActivity;
 
 import java.util.List;
 
@@ -18,10 +24,13 @@ import java.util.List;
  */
 public class MyExpenseRecyclerViewAdapter extends RecyclerView.Adapter<MyExpenseRecyclerViewAdapter.ViewHolder> {
 
-    private final List<DummyItem> mValues;
+    private final List<Expense> mValues;
+    private boolean mTwoPane;
+    private FragmentActivity activity;
 
-    public MyExpenseRecyclerViewAdapter(List<DummyItem> items) {
+    public MyExpenseRecyclerViewAdapter(List<Expense> items, FragmentActivity activity) {
         mValues = items;
+        this.activity = activity;
     }
 
     @Override
@@ -33,9 +42,13 @@ public class MyExpenseRecyclerViewAdapter extends RecyclerView.Adapter<MyExpense
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
         holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
+        holder.mAmountView.setText("$" + mValues.get(position).amount.toString());
+        holder.mCategory.setText(mValues.get(position).category.toString());
+
+        holder.itemView.setTag(mValues.get(position));
+        holder.itemView.setOnClickListener(mOnClickListener);
     }
 
     @Override
@@ -43,22 +56,39 @@ public class MyExpenseRecyclerViewAdapter extends RecyclerView.Adapter<MyExpense
         return mValues.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public DummyItem mItem;
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView mIdView;
+        final TextView mContentView;
+        final TextView mAmountView;
+        final TextView mCategory;
 
-        public ViewHolder(View view) {
+        ViewHolder(View view) {
             super(view);
-            mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mIdView = (TextView) view.findViewById(R.id.id_text);
             mContentView = (TextView) view.findViewById(R.id.content);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            mAmountView = (TextView) view.findViewById(R.id.amount);
+            mCategory = (TextView) view.findViewById(R.id.category);
         }
     }
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Expense item = (Expense) view.getTag();
+            if (mTwoPane) {
+//                Bundle arguments = new Bundle();
+//                arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
+//                ItemDetailFragment fragment = new ItemDetailFragment();
+//                fragment.setArguments(arguments);
+//                mParentActivity.getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.item_detail_container, fragment)
+//                        .commit();
+            } else {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, ItemDetailActivity.class);
+                intent.putExtra("item_id", item.id);
+
+                context.startActivity(intent);
+            }
+        }
+    };
 }
