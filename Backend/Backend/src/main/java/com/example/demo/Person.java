54794f5633770d07ab20a7292af55341f1d243d2
mamoore <mamoore@iastate.edu>
2020-10-06 13:07:36 -0500

    friends table added

--- Backend/Backend/src/main/java/com/example/demo/Person.java
+++ Backend/Backend/src/main/java/com/example/demo/Person.java
@@ -36,6 +36,20 @@ class Person {
 	inverseJoinColumns = @JoinColumn(name = "subscription_id"))
 	private List<Subscription> subscriptionsBought;
 	
+	@ManyToMany
+	@JoinTable(name="tbl_friends",
+	 joinColumns=@JoinColumn(name="userId"),
+	 inverseJoinColumns=@JoinColumn(name="friendId")
+	)
+	private List<Person> friends;
+
+	@ManyToMany
+	@JoinTable(name="tbl_friends",
+	 joinColumns=@JoinColumn(name="friendId"),
+	 inverseJoinColumns=@JoinColumn(name="userId")
+	)
+	private List<Person> friendOf;
+	
 	//private ArrayList<String> friendId = new ArrayList<String>();
 	
 	public int getId() { return id; }
@@ -47,5 +61,6 @@ class Person {
 	public void setItemsBought(List<ItemAdd> items) { itemsBought = items; }
 	public String toString() { return username; }
 	public List<Subscription> getSubscriptionsBought() { return subscriptionsBought;}
-	
+	public List<Person> getFriends() { return friends; }
+	public List<Person> getFriendsOf() { return friendOf; }
 }
