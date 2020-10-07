2297ba06f75cae74da40c66a1a6a06befc43f718
Kateryna <kateryna@iastate.edu>
2020-10-07 18:05:02 -0500

    Split summary list view and displayed a bar graph on the top half.

--- Frontend/app/src/main/java/iastate/cs309/myexpenses/BarGraphFragment.java
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/BarGraphFragment.java
@@ -2,18 +2,28 @@ package iastate.cs309.myexpenses;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BarGraphFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BarGraphFragment extends Fragment {
+public class BarGraphFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +33,8 @@ public class BarGraphFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private BarChart chart;
+    private SeekBar seekBarX, seekBarY;
 
     public BarGraphFragment() {
         // Required empty public constructor
@@ -53,12 +65,104 @@ public class BarGraphFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bar_graph, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_bar_graph, container, false);
+
+        chart = rootView.findViewById(R.id.chart1);
+
+        seekBarX = rootView.findViewById(R.id.seekBar1);
+        seekBarY = rootView.findViewById(R.id.seekBar2);
+        seekBarX.setOnSeekBarChangeListener(this);
+        seekBarY.setOnSeekBarChangeListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+//        tvX.setText(String.valueOf(seekBarX.getProgress()));
+//        tvY.setText(String.valueOf(seekBarY.getProgress()));
+
+        setData(seekBarX.getProgress(), seekBarY.getProgress());
+        chart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    private void setData(int count, float range) {
+
+        float start = 1f;
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        for (int i = (int) start; i < start + count; i++) {
+            float val = (float) (Math.random() * (range + 1));
+
+            if (Math.random() * 100 < 25) {
+                values.add(new BarEntry(i, val, getResources().getDrawable(R.drawable.ic_baseline_friends_24)));
+            } else {
+                values.add(new BarEntry(i, val));
+            }
+        }
+
+        BarDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+
+        } else {
+            set1 = new BarDataSet(values, "The year 2017");
+
+            set1.setDrawIcons(false);
+
+            int startColor1 = ContextCompat.getColor(getContext(), android.R.color.holo_orange_light);
+            int startColor2 = ContextCompat.getColor(getContext(), android.R.color.holo_blue_light);
+            int startColor3 = ContextCompat.getColor(getContext(), android.R.color.holo_orange_light);
+            int startColor4 = ContextCompat.getColor(getContext(), android.R.color.holo_green_light);
+            int startColor5 = ContextCompat.getColor(getContext(), android.R.color.holo_red_light);
+            int endColor1 = ContextCompat.getColor(getContext(), android.R.color.holo_blue_dark);
+            int endColor2 = ContextCompat.getColor(getContext(), android.R.color.holo_purple);
+            int endColor3 = ContextCompat.getColor(getContext(), android.R.color.holo_green_dark);
+            int endColor4 = ContextCompat.getColor(getContext(), android.R.color.holo_red_dark);
+            int endColor5 = ContextCompat.getColor(getContext(), android.R.color.holo_orange_dark);
+
+//            List<Fill> gradientFills = new ArrayList<>();
+//            gradientFills.add(new Fill(startColor1, endColor1));
+//            gradientFills.add(new Fill(startColor2, endColor2));
+//            gradientFills.add(new Fill(startColor3, endColor3));
+//            gradientFills.add(new Fill(startColor4, endColor4));
+//            gradientFills.add(new Fill(startColor5, endColor5));
+//
+//            set1.setFsetFills(gradientFills);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            //data.setValueTypeface(tfLight);
+            data.setBarWidth(0.9f);
+
+            chart.setData(data);
+        }
     }
 }
