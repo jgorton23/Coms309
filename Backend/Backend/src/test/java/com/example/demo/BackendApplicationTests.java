66668537f5fc523c529b8d9b111631872b843071
Jacob Gorton <jagorton@iastate.edu>
2020-10-21 00:20:00 -0500

    test yml

--- Backend/Backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ Backend/Backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -3,27 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// maybe add h2 dependency with scope <test>
-
-/*
-@RunWith(SpringRunner.class) 
-@DataJpaTest //changed to JpaTest according to tutorial
-public class BackendApplicationTests {
-
-	@Autowired
-	private TestEntityManager entityManager; // only set up to test on MyDatabase, add more autowiring for item or subscritption
-
-	@Autowired
-	private MyDatabase myDatabase;
-
-	@Autowired
-	private MyController myController; //idk if this makes sense, but the tutorial didnt use a controller and idk if i have to autowire it
+@SpringBootTest
+class BackendApplicationTests {
 
 	@Test
-	public void test1() { //change name to be more descriptive maybe, or keep coments
-		 //write test here
+	void contextLoads() {
 	}
-
-	//write more tests here
 
 }
