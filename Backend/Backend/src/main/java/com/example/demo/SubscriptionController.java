81d53a97c2a46e47a291df3d966aa0c78da63173
Jacob Gorton <jagorton@iastate.edu>
2020-10-26 11:51:23 -0500

    subDB

--- Backend/Backend/src/main/java/com/example/demo/SubscriptionController.java
+++ Backend/Backend/src/main/java/com/example/demo/SubscriptionController.java
@@ -25,7 +25,7 @@ public class SubscriptionController {
 	
 	@RequestMapping("/getSubscriptions/{id}")
 	List<Subscription> findAllItemsByID(@PathVariable int id) {
-		List<Integer> i = [1,2];
+		List<Integer> i = Arrays.asList(1,2);
 		return subDB.findAllById(i);
 	}
 	
