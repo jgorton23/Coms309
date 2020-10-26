69903e168d4a3e538e661bbfc8d60d859cb8dba0
Polina <psemenova0201@gmail.com>
2020-10-26 11:40:42 -0500

    Connect Registration to Login screen and vise versa

--- Frontend/app/src/main/java/iastate/cs309/myexpenses/login/RegisterActivity.java
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/login/RegisterActivity.java
@@ -2,15 +2,40 @@ package iastate.cs309.myexpenses.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import iastate.cs309.myexpenses.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText)findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button)findViewById(R.id.button_register);
+        mTextViewLogin= (TextView)findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
     }
 }
