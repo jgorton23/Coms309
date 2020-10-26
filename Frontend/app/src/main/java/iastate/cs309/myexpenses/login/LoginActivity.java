74f5f3fce286592092256a60a341584fda37161c
Polina <psemenova0201@gmail.com>
2020-10-26 11:19:07 -0500

    Create a Login Activity Screen

---
+++ Frontend/app/src/main/java/iastate/cs309/myexpenses/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package iastate.cs309.myexpenses.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import iastate.cs309.myexpenses.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
