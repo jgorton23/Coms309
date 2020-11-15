5407c9ec689cce8c2e68969bf2e4fc86154e5891
mamoore <mamoore@iastate.edu>
2020-11-15 13:25:02 -0600

    nothign changed

--- Backend/Backend/src/main/java/com/example/demo/MyController.java
+++ Backend/Backend/src/main/java/com/example/demo/MyController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @RestController
 public class MyController {
 
