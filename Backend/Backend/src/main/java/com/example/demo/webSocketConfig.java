98af6ae312b6f91e4efd3e5a64ff60c8ed9f9be2
Jacob Gorton <jagorton@iastate.edu>
2020-11-14 18:02:46 -0600

    config file added

---
+++ Backend/Backend/src/main/java/com/example/demo/webSocketConfig.java
@@ -0,0 +1,14 @@
+package com.cs309.WebSocketsExample.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class webSocketConfig {
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+}
